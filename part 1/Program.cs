8b6ad9ec8cbf27bf34ea7c2bd2f79b2b156383d1
tata <t.iravany83@gmail.com>
2025-10-15 12:06:37 +0330

    Add project files.

---
+++ part 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace part_1
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("your number is Even :) ");
+                }
+                else
+                {
+                    Console.WriteLine("your number is Odd!");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" EROR! must be number !!");
+            }
+
+            Console.WriteLine("Tap 'Enter' to close ...");
+            Console.ReadLine();
+        }
+    }
+}
